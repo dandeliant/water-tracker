85e675ffdf151d58459e155edd867a821cd7b89a
dandeliant <74840084+dandeliant@users.noreply.github.com>
2025-10-23 19:49:03 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,39 @@
-const CACHE = 'water-v1';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE = 'water-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
-self.addEventListener('fetch', e => {
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      if (e.request.method==='GET' && new URL(e.request.url).origin===location.origin) {
-        const clone = res.clone(); caches.open(CACHE).then(c=>c.put(e.request, clone));
-      }
-      return res;
-    }).catch(()=>caches.match('./index.html')))
+    caches.match(req).then(cacheRes =>
+      cacheRes ||
+      fetch(req).then(netRes => {
+        // cache only GET & same-origin
+        if (req.method === 'GET' && new URL(req.url).origin === location.origin) {
+          const clone = netRes.clone();
+          caches.open(CACHE).then(c => c.put(req, clone));
+        }
+        return netRes;
+      }).catch(() => caches.match('./index.html'))
+    )
   );
 });
